685141a1aa78c731a16adbdb07dd533ef3a772cc
Hienmv94 <hienmv94@gmail.com>
2015-09-06 23:38:04 +0700

    Add user following

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,3 +1,5 @@
+
+
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
@@ -7,7 +9,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   else
     storage :file
   end
-
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
