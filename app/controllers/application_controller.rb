b3e5e6d25ca973c694283d65d070e23b7c0e9ca0
Hienmv94 <hienmv94@gmail.com>
2016-01-19 14:32:17 +0700

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-
+  include SessionsHelper
   def current_user
     if User.find_by id: session[:user_id]
       @current_user ||= User.find session[:user_id]
     end
+  end
+  def current_user?(user)
+    user == current_user
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,10 @@
 module SessionsHelper
+  def current_user
+    if User.find_by id: session[:user_id]
+      @current_user ||= User.find session[:user_id]
+    end
+  end
+  def current_user?(user)
+    user == current_user
+  end
 end
